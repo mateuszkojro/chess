7c8b73354a98c5de6d12f31d02159acbf6ef28c1
mateusz kojro <mateusz.kojro@unilodz.eu>
2020-01-18 00:35:51 +0000

    done: -docker compose -better testing with server and client -start docker container with run_server.sh

--- client.py
+++ client.py
@@ -3,17 +3,17 @@ import sys
 
 s = socket.socket()
 
-ip = '127.0.0.1'
+ip = "217.113.232.164"
 port = 3333
 
 s.connect(( ip, port)) #polaczenie pod adresem ip i portem port
 
+msg = sys.argv[1] #tworze wiadomosc do wyslania jakos string
+
+s.send( bytes(msg,'utf-8') ) #konwertuje na bajty i wysylam
+
 x = s.recv(1024) #odbieram 1024 bajty
 x = x.decode() #dekoduje do str
-
 print(x)
 
-msg = sys.argv[1],'utf-8' #tworze wiadomosc do wyslania jakos string
-
-s.send( bytes(msg) ) #konwertuje na bajty i wysylam
 s.close()
